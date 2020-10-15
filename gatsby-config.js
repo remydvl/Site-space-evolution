259784df2e82c4416c05535d13ff6868aa00b8a6
Billy Ros <billyros@gmail.com>
2020-10-15 16:49:25 +0200

    Add CMS Data to home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,5 +13,20 @@ module.exports = {
         component: require.resolve(`${__dirname}/src/layouts/index.tsx`),
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/assets`,
+        name: 'images',
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    'gatsby-transformer-remark',
   ],
 }
